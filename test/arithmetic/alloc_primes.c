67d658a867edc6a26ccbc99c4b8b9b5dc910d2c0
tolis-0 <yotravik@gmail.com>
2023-10-27 00:01:39 +0300

    alloc primes test template

--- test/arithmetic/alloc_primes.c
+++ test/arithmetic/alloc_primes.c
@@ -1,1 +1,21 @@
+#include "../dctest.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main(int argc, char **argv)
+{
+	uint32_t *primes;
+
+	test_arguments_setup();
+
+	mandatory_tests {
+
+	}
+
+	perf_tests {
+
+	}
+
+	return 0;
+}
