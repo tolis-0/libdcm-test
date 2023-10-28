0ae13af524258c43465197777d7571421ef9c16f
tolis-0 <yotravik@gmail.com>
2023-10-28 13:05:22 +0300

    added exp_mod tests

--- test/arithmetic/exp_mod.c
+++ test/arithmetic/exp_mod.c
@@ -5,15 +5,14 @@
 #include <string.h>
 
 
-GMP_TEST_BEGIN(add_mod, 0b1010)
-mpz_add(gmp_r, gmp_a, gmp_b);
-mpz_mod(gmp_r, gmp_r, gmp_m);
-r = dc_add_mod(a, b, m);
-GMP_TEST_END(add_mod, 0b1010)
+GMP_TEST_BEGIN(exp_mod, 0b1010)
+mpz_powm(gmp_r, gmp_a, gmp_b, gmp_m);
+r = dc_exp_mod(a, b, m);
+GMP_TEST_END(exp_mod, 0b1010)
 
-MODULO_PERFORMANCE_START(add_mod, 0b1010)
-output = dc_add_mod(a[i], b[i], m[i]);
-MODULO_PERFORMANCE_END(add_mod, 0b1010)
+MODULO_PERFORMANCE_START(exp_mod, 0b1010)
+output = dc_exp_mod(a[i], b[i], m[i]);
+MODULO_PERFORMANCE_END(exp_mod, 0b1010)
 
 
 int main(int argc, char **argv)
@@ -24,18 +23,18 @@ int main(int argc, char **argv)
 	mandatory_tests {
 		for (i = 1; i <= 64; i++)
 			for (j = 8; j <= 64; j += 8)
-				add_mod_gmp_test(25000, j, i);
+				exp_mod_gmp_test(25000, j, i);
 	}
 
 	extra_tests {
 		for (i = 1; i <= 64; i++)
 			for (j = 8; j <= 64; j += 8)
-				add_mod_gmp_test(1000000, j, i);
+				exp_mod_gmp_test(1000000, j, i);
 	}
 
 	perf_tests {
 		for (int i = 1; i <= 64; i++)
-			add_mod_performance(100000, i);
+			exp_mod_performance(100000, i);
 	}
 
 	return 0;
