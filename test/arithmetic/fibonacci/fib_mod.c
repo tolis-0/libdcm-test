2644f93bf3baca2c07ff7dc06038b0ad086f43af
tolis-0 <yotravik@gmail.com>
2023-11-06 15:35:35 +0200

    updated fib_mod tests

--- test/arithmetic/fibonacci/fib_mod.c
+++ test/arithmetic/fibonacci/fib_mod.c
@@ -1,4 +1,5 @@
 #include "../../dctest.h"
+#include "../../gmptest.h"
 #include <string.h>
 #include <stdlib.h>
 
@@ -7,6 +8,12 @@
 #define EXTRA_N 2000000
 
 #pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+
+GMP_TEST_BEGIN(fib_mod, 0b0010)
+mpz_fib_ui(gmp_r, a);
+mpz_mod(gmp_r, gmp_r, gmp_m);
+r = dc_fib_mod(a, m);
+GMP_TEST_END(fib_mod, 0b0010)
 
 void dc_fib_mod_test(int t, uint64_t n_start, uint64_t f_0, uint64_t f_1, uint64_t m);
 void dc_fib_mod_performance (uint64_t total, uint32_t bits, uint32_t divbits);
@@ -18,6 +25,14 @@ int main(int argc, char **argv)
 	test_arguments_setup();
 
 	mandatory_tests {
+		for (i = 1; i <= 64; i++) {
+				fib_mod_gmp_test(50000, 4, i);
+				fib_mod_gmp_test(20000, 8, i);
+				fib_mod_gmp_test(5000, 12, i);
+				fib_mod_gmp_test(2000, 16, i);
+				fib_mod_gmp_test(200, 20, i);
+		}
+
 		dc_fib_mod_test(MAND_N, 103948543, 8273437, 99265733, 100000000);
 		dc_fib_mod_test(MAND_N, 207897089, 32316189, 56812120, 100000000);
 		dc_fib_mod_test(MAND_N, 1002344234, 59238737, 73373890, 100000000);
@@ -27,10 +42,17 @@ int main(int argc, char **argv)
 		dc_fib_mod_test(MAND_N, 5421874925, 220420823, 280553549, 987365031);
 		dc_fib_mod_test(MAND_N, 10843749850, 541264870, 279115409, 987365031);
 
+		dc_fib_mod_test(MAND_N, 3804736455, 5137179646, 11398095651, 15523659327);
 	}
 
 	extra_tests {
-
+		for (i = 1; i <= 64; i++) {
+				fib_mod_gmp_test(1000000, 4, i);
+				fib_mod_gmp_test(500000, 8, i);
+				fib_mod_gmp_test(50000, 12, i);
+				fib_mod_gmp_test(20000, 16, i);
+				fib_mod_gmp_test(5000, 20, i);
+		}
 	}
 
 	perf_tests {
@@ -52,9 +74,9 @@ void dc_fib_mod_test(int t, uint64_t n, uint64_t f_0, uint64_t f_1, uint64_t m)
 	uint64_t output, tmp;
 
 	output = dc_fib_mod(n, m);
-	_test_check(uint64_t, uint64_t, n, f_0, output);
+	_test_check2(uint64_t, uint64_t, uint64_t, n, m, f_0, output);
 	output = dc_fib_mod(++n, m);
-	_test_check(uint64_t, uint64_t, n, f_1, output);
+	_test_check2(uint64_t, uint64_t, uint64_t, n, m, f_1, output);
 
 	for (n++; i < t; i++, n++) {
 		tmp = dc_add_mod(f_0, f_1, m);
